c00c02aabbfb71bd99284c986f1306e941da3226
Clive4216 <110213545+Clive4216@users.noreply.github.com>
2024-08-24 20:47:30 +0530

    Update viewmap1.js

--- viewmap1.js
+++ viewmap1.js
@@ -19,7 +19,7 @@ function onMapClick(e)
 map.on('click',onMapClick);
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC3fErJkfj1XMyyz8Ir9fnvWrf9diAnIJU",
+    apiKey: "YOUR_API_KEY",
     authDomain: "comevent-467db.firebaseapp.com",
     databaseURL: "https://comevent-467db-default-rtdb.firebaseio.com",
     projectId: "comevent-467db",
